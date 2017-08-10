ff00bc07a452866f876d0c5a365bb7151cf20ed7
sanwax <>
2017-08-10 22:12:26 +0200

    Reviewed tests: added ctest target and allUnitTest target.

--- src/libCore/test/FactoryHoldingTest.cpp
+++ src/libCore/test/FactoryHoldingTest.cpp
@@ -2,17 +2,21 @@
 
 namespace core
 {
-  namespace test
-  {
-    void init()
-    {
-      static int i=0;
-    }
+	namespace test
+	{
 
-    TEST(FactoryHolding,TriggerTest)
-    {
-      EXPECT_EQ(1,1)<<"Must work!";
-    }
 
-  }
+		void init()
+		{
+			static int i = 0;
+		}
+
+
+		TEST(core_FactoryHolding, TriggerTest)
+		{
+			EXPECT_EQ(1, 1) << "Must work!";
+		}
+
+
+	}
 }
